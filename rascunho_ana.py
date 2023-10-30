cddfa27162bdd7219ff7d635194946ab6f8c291f
Ana Júlia Amaro Pereira Rocha <B51991@fgv.edu.br>
2023-10-30 17:11:52 -0300

    adiciona Enum das marcas

--- rascunho_ana.py
+++ rascunho_ana.py
@@ -1,6 +1,19 @@
 from enum import Enum
 import aspose.barcode as barcode
 
+class Marca(Enum):
+    NESTLE = 101
+    GAROTO = 102
+    MARISA = 201
+    RENNER = 202
+    FANTA = 301
+    COCA_COLA = 302
+    GUARANA = 303
+    
+    def __str__(self):
+        return self.name
+    
+
 class Produto():
     id_produto = 1
 
@@ -21,12 +34,11 @@ class Produto():
     
     def salvar_codigo_barras(self, path:str="./"):
         self.codigo_de_barras.save(f"{path}cod_barras_{self.nome}_{self.id_produto}.png")
-
+    
 
 class Roupa(Produto):
-    def __init__(self, nome, valor, marca_produto, tipo_roupa, tipo_tecido):
+    def __init__(self, nome, valor, marca_produto, tipo_tecido):
         super().__init__(nome, valor, marca_produto)
-        self.tipo_roupa = tipo_roupa
         self.tipo_tecido = tipo_tecido
 
 
@@ -43,25 +55,27 @@ class Refrigerante(Produto):
         self.tipo_recipiente = tipo_recipiente
         self.sabor = sabor
 
-    
-class id_marca(Enum):
-    Yoki = 34
-    Nestle = 44
-    
-print(id_marca)
 
-prod_1 = Chocolate("Talento", 8, "Nestle", "Maracujá", 80)
-prod_2 = Chocolate("Barra de Chocolate", 6.5, "Garoto", "Chocolate Amargo", 75)
-prod_3 = Chocolate("Bombom Serenata", 1, "Garoto", "Amendoim", 10)
+prod_1 = Chocolate("Talento", 8, Marca.NESTLE, "Maracujá", 80)
+prod_2 = Chocolate("Barra de Chocolate", 6.5, Marca.GAROTO, "Chocolate Amargo", 75)
+prod_3 = Chocolate("Bombom Serenata", 1, Marca.GAROTO, "Amendoim", 10)
 
-prod_4 = Roupa("Calça", 120, 546372, "Marisa", "jeans")
-prod_5 = Roupa("Vestido", 90, 547384, "Renner", "Malha Estampada")
-prod_6 = Roupa("Jaqueta", 240, 336475, "C&A", "Couro")
+prod_4 = Roupa("Calça", 120, Marca.MARISA, "jeans")
+prod_5 = Roupa("Vestido", 90, Marca.RENNER, "Malha Estampada")
+prod_6 = Roupa("Jaqueta", 240, Marca.RENNER, "Couro")
 
-prod_7 = Refrigerante("Fanta Laranja", 7, 546374, "Fanta", "lata", "laranja")
-prod_8 = Refrigerante("Coca-Cola", 12, 647583, "Coca-Cola", "KS", "Coca-Cola")
-prod_9 = Refrigerante("Guaraná", 9, 647583, "Guaraná", "garrafa plástica", "Guaraná")
+prod_7 = Refrigerante("Fanta Laranja", 7, Marca.FANTA, "lata", "laranja")
+prod_8 = Refrigerante("Coca-Cola", 12, Marca.COCA_COLA, "KS", "Coca-Cola")
+prod_9 = Refrigerante("Guaraná", 9, Marca.GUARANA, "garrafa plástica", "Guaraná")
 
-print(prod_1.nome)
-        
-    
+'''
+print(prod_1)
+print(prod_2)
+print(prod_3)
+print(prod_4)
+print(prod_5)
+print(prod_6)
+print(prod_7)
+print(prod_8)
+print(prod_9)
+'''
