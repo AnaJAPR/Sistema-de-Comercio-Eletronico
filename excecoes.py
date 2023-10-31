0fc8d76d26f71d4e63bf41d5b4ec39147e7dae78
Ana Júlia Amaro Pereira Rocha <B51991@fgv.edu.br>
2023-10-30 21:02:03 -0300

    devolvendo atualizações de exceção

--- excecoes.py
+++ excecoes.py
@@ -4,23 +4,35 @@ class DeuRuimError(ZeroDivisionError):
         self.message = message
         super().__init__(self.message)
 
-
 '''
 
 class TipoIncorretoError(TypeError):
     def __init__(self, message="Tipo de dado inserido está incorreto, tente olhar a documentação e inserir novamente.") -> None:
         self.message = message
         super().__init__(self.message)
-        
-# teste da exceção criada acima
-def teste():
-    raise TipoIncorretoError()
 
-try:
-    teste()
 
-except TipoIncorretoError as e:
-    print(e.message)
+class PrecoNegativoError(ValueError):
+    def __init__(self, message="Preço negativo inválido, o preço do produto deve ser maior ou igual a 0 para que a operação seja válida.") -> None:
+        self.message = message
+        super().__init__(self.message)
 
+
+class MarcaInvalidaError(KeyError):
+    def __init__(self, message="A marca é inválida, tente conferir quais marcas são válidas e inserir uma nova.") -> None:
+        self.message = message
+        super().__init__(self.message)
+
+
+if __name__ == "__main__":
+    # teste da exceção criada acima
+    def teste():
+        raise TipoIncorretoError()
+
+    try:
+        teste()
+
+    except TipoIncorretoError as e:
+        print(e.message)
 
 
